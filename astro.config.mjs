41874b6416070e5d49083fb4612229a339d8a7da
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-11-30 03:09:49 +0900

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,34 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import starlightBlogPlugin from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      sidebar: [
+        {
+          label: "blog/2024",
+          autogenerate: { directory: "2024" },
+        },
+      ],
+      title: "晴れときどき崩壊の日々",
+      social: {
+        github: "https://github.com/ojii3",
+        "x.com": "https://x.com/ojii3dev",
+      },
+      logo: {
+        src: "./src/assets/icon.png",
+      },
+      favicon: "/favicon.ico",
+      customCss: ["./src/styles/custom.css", "./src/styles/tailwind.css"],
+      plugins: [starlightBlogPlugin()],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
