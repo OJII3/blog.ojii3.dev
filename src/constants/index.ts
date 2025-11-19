ccfda6c14925b9cedce7bf596a93cc8d04840c6a
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-11-19 18:06:03 +0900

    fix: h1 style

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,20 +1,32 @@
 export const SITE_URL = "https://blog.ojii3.dev";
 export const SITE_NAME = "晴れときどき崩壊ブログ";
-export const BORDER_COLORS = [
-  "border-rose-500",
-  "border-orange-500",
-  "border-yellow-500",
-  "border-lime-500",
-  "border-emerald-500",
-  "border-indigo-500",
-  "border-purple-500",
-];
-export const BG_COLORS = [
-  "bg-rose-500",
-  "bg-orange-500",
-  "bg-yellow-500",
-  "bg-lime-500",
-  "bg-emerald-500",
-  "bg-indigo-500",
-  "bg-purple-500",
-];
+
+export enum VitaColor {
+  Red,
+  Orange,
+  Yellow,
+  Green,
+  Blue,
+  Indigo,
+  Violet,
+}
+
+export const BORDER_COLORS: Record<VitaColor, string> = {
+  [VitaColor.Red]: "border-rose-500",
+  [VitaColor.Orange]: "border-orange-500",
+  [VitaColor.Yellow]: "border-yellow-500",
+  [VitaColor.Green]: "border-lime-500",
+  [VitaColor.Blue]: "border-emerald-500",
+  [VitaColor.Indigo]: "border-indigo-500",
+  [VitaColor.Violet]: "border-purple-500",
+};
+
+export const BG_COLORS: Record<VitaColor, string> = {
+  [VitaColor.Red]: "bg-rose-500",
+  [VitaColor.Orange]: "bg-orange-500",
+  [VitaColor.Yellow]: "bg-yellow-500",
+  [VitaColor.Green]: "bg-lime-500",
+  [VitaColor.Blue]: "bg-emerald-500",
+  [VitaColor.Indigo]: "bg-indigo-500",
+  [VitaColor.Violet]: "bg-purple-500",
+};
--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,12 +1,12 @@
-import { BG_COLORS, BORDER_COLORS } from "../constants";
+import { BG_COLORS, BORDER_COLORS, type VitaColor } from "../constants";
 
 export const getBorderColorByDate = (date: Date): string => {
   const colorIndex = date.getDate() % 7; // 0-6 for 7 colors
-  return BORDER_COLORS[colorIndex];
+  return BORDER_COLORS[colorIndex as VitaColor];
 };
 export const getBgColorByDate = (date: Date): string => {
   const colorIndex = date.getDate() % 7; // 0-6 for 7 colors
-  return BG_COLORS[colorIndex];
+  return BG_COLORS[colorIndex as VitaColor];
 };
 export const getColorIndexByDate = (date: Date): number => {
   return date.getDate() % 7; // 0-6 for 7 colors
