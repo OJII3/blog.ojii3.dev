d58c810b5634cb536eb05e9c1a8a2d4d3c762b26
OJII3 <84656786+OJII3@users.noreply.github.com>
2026-08-16 13:13:34 +0900

    fix: bundle Satori Yoga WASM for OG images (#65)

--- src/types/wasm.d.ts
+++ src/types/wasm.d.ts
@@ -2,3 +2,8 @@ declare module "@resvg/resvg-wasm/index_bg.wasm" {
 	const wasmModule: WebAssembly.Module;
 	export default wasmModule;
 }
+
+declare module "satori/yoga.wasm" {
+	const wasmModule: WebAssembly.Module;
+	export default wasmModule;
+}
